001d36da4def070d141190d768f2bed831108aa7
Samira <samira.baecker@informatik.hs-fulda.de>
2024-02-26 22:07:08 +0100

    it is now possible to create file models from json and convert them to json, there should be something like get all lower files or something like this... or get all list things something like this

--- lib/project/textfield.dart
+++ lib/project/textfield.dart
@@ -2,49 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:whereitis_2/project/textstyle.dart';
 
 class ProjectTextFieldWidget {
-  static Widget inputField1(String title, TextEditingController controller, bool editMode) =>
-      Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      child: Text(
-                        "$title:",
-                        style: ProjectTextStyle.normal2(),
-                      ),
-                    ),
-                  ),
-                ),
+  static Widget inputField1(String title, TextEditingController controller, bool editMode, {InputDecoration? decoration}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "$title:",
+            style: ProjectTextStyle.normal2(),
+          ),
+          LayoutBuilder(
+            builder: (context, constraints) => Container(
+              width: constraints.maxWidth, // Set the width to match the parent width
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(color: Colors.grey), // Border color
               ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 320,
-                  child: editMode
-                      ? TextField(
-                          controller: controller,
-                          style: ProjectTextStyle.normal2(),
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.only(bottom: 15, top: 18),
-                          child: Text(
-                            controller.text,
-                            style: ProjectTextStyle.normal2(),
-                          ),
-                        ),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                child: editMode
+                    ? TextFormField(
+                  controller: controller,
+                  style: ProjectTextStyle.normal2(),
+                  decoration: decoration,
+                )
+                    : Text(
+                  controller.text,
+                  style: ProjectTextStyle.normal2(),
                 ),
               ),
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
+  }
+
+  static InputDecoration inputDecorationForLogin(String labelText, String hintText, IconData prefix) {
+    return InputDecoration(
+      labelText: labelText, // Placeholder text
+      hintText: hintText,
+      prefixIcon: Icon(prefix), // Icon before the text input
+      suffixIcon: Icon(Icons.clear), // Icon after the text input
+      border: OutlineInputBorder(
+        // Border outline
+        borderRadius: BorderRadius.circular(10),
+      ),
+      fillColor: Colors.black12,
+      filled: true,
+    );
+  }
 }
